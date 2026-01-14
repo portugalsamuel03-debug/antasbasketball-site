e31f39a2fb0a4ffd9704e61af88fd32b9e05d2ef
portugalsamuel03-debug <portugalsamuel03@gmail.com>
2026-01-14 06:39:45 -0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../App";
+import "../index.css";
+
+const el = document.getElementById("root");
+if (!el) {
+  document.body.innerHTML =
+    "<pre style='color:white;padding:16px'>#root não encontrado</pre>";
+} else {
+  ReactDOM.createRoot(el).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+console.log("✅ main.tsx carregou");
